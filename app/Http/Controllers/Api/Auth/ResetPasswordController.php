637ce46199a1c9ff1135b26ba1bd3d60fd83158d
osamarafa1994 <osama@motaweron.com>
2023-01-18 14:36:32 +0200

    first commit

---
+++ app/Http/Controllers/Api/Auth/ResetPasswordController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\ResetCodePassword;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ResetPasswordController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'code' => 'required|string|exists:reset_code_passwords',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+        if($validator->fails()) {
+            return helperJson(null, $validator->errors(), 422);
+        }
+
+        // find the code
+        $passwordReset = ResetCodePassword::firstWhere('code', $request->code);
+
+        // check if it does not expired: the time is one hour
+        if ($passwordReset->created_at->addHour() < now()) {
+            $passwordReset->delete();
+            return helperJson(null, ['message' => trans('passwords.code_is_expire')], 422);
+        }
+
+        // find user's email
+        $user = User::firstWhere('email', $passwordReset->email);
+
+        // update user password
+        $user->update($request->only('password'));
+
+        // delete current code
+        $passwordReset->delete();
+
+        return response(['message' =>'password has been successfully reset'], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Provider\CategoryController;
+use App\Http\Controllers\Api\Provider\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['namespace' => 'Auth','prefix' => 'auth'],function (){
+//    Route::post('password/email',  ForgotPasswordController::class);
+//    Route::post('password/code/check', CodeCheckController::class);
+//    Route::post('password/reset', ResetPasswordController::class);
+    Route::post('login',[AuthController::class, 'login']);
+    Route::POST('register',[AuthController::class, 'register']);
+    Route::POST('update-profile',[AuthController::class, 'update_profile']);
+    Route::POST('delete-account',[AuthController::class, 'deleteAccount']);
+    Route::get('my-profile',[AuthController::class, 'me']);
+//    Route::post('insert-token',[NotificationController::class, 'insert_token']);
+});
+Route::group(['namespace' => 'Provider','prefix' => 'categories'],function (){
+    Route::get('list', [CategoryController::class, 'index']);
+    Route::post('store', [CategoryController::class, 'store']);
+    Route::post('update/{id}', [CategoryController::class, 'update']);
+    Route::post('delete/{id}', [CategoryController::class, 'destroy']);
+});
+
+Route::group(['namespace' => 'Provider','prefix' => 'products'],function (){
+    Route::get('list', [ProductController::class, 'index']);
+    Route::post('store', [ProductController::class, 'store']);
+    Route::post('update/{id}', [ProductController::class, 'update']);
+    Route::post('delete/{id}', [ProductController::class, 'destroy']);
+});
+
+
+Route::group([ 'middleware' => 'api','namespace' => 'Api'], function () {
+
+});
+
+

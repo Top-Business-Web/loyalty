d4d2aea48b42066359b39c0bce7be6a91685240e
osamarafa1994 <osama@motaweron.com>
2023-02-01 14:12:55 +0200

    categories & Products By Id geyt

--- routes/api.php
+++ routes/api.php
@@ -59,7 +59,7 @@ Route::group(['prefix' => 'provider/orders'],function (){
 
 Route::group([ 'middleware' => 'api','namespace' => 'Api'], function () {
     Route::get('setting',[SettingController::class, 'index']);
-    Route::post('/paytap/store',[PaytapsPaymentController::class,'store'])->name('paytap');
+    Route::get('/paytap/store',[PaytapsPaymentController::class,'store'])->name('paytap');
     Route::get('/paytap_home',[PaytapsPaymentController::class,'callback_tap'])->name('callback_tap');
     Route::get('/return_paytap',[PaytapsPaymentController::class,'return_paytap'])->name('return_paytap');
 });

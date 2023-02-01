08758c0606551f4e3b8f5131d824f5231f41e4db
osamarafa1994 <osama@motaweron.com>
2023-02-01 15:02:23 +0200

    categories & Products Bss

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,7 @@ Route::group(['prefix' => 'provider/orders'],function (){
 Route::group([ 'middleware' => 'api','namespace' => 'Api'], function () {
     Route::get('setting',[SettingController::class, 'index']);
     Route::get('/paytap/store',[PaytapsPaymentController::class,'store'])->name('paytap');
-    Route::get('/paytap_home',[PaytapsPaymentController::class,'callback_tap'])->name('callback_tap');
+//    Route::get('/paytap_home',[PaytapsPaymentController::class,'callback_tap'])->name('callback_tap');
     Route::get('/return_paytap',[PaytapsPaymentController::class,'return_paytap'])->name('return_paytap');
 
 });

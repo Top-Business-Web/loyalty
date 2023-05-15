d17953cdd113e60ffdc5c282644ab61715a5bcaa
osamarafa1994 <osama@motaweron.com>
2023-05-15 15:16:23 +0300

    orsders in client

--- app/Services/Api/Client/ProductService.php
+++ app/Services/Api/Client/ProductService.php
@@ -16,10 +16,10 @@ class ProductService
     use DefaultImage,GeneralTrait;
     public function productsByCategoryId($request,$id){
 //        $user = auth()->user();
-        $data['the_best'] = Product::where(['category_id'=> $id,'the_best'=> '1'])->get();
-        $data['products'] = Product::where('category_id', $id)->get();
+        $data['the_best'] = ProductResource::collection(Product::where(['category_id'=> $id,'the_best'=> '1'])->get());
+        $data['products'] = ProductResource::collection(Product::where('category_id', $id)->get());
 
-        return helperJson(ProductResource::collection($data), '');
+        return helperJson($data, '');
 
     }
 }

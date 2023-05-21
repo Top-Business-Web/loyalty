c29044b8646f7a92c79d747502aa1d75250a2d8a
osamarafa1994 <osama@motaweron.com>
2023-05-21 10:58:55 +0300

    packages

--- app/Services/Api/Client/HomeService.php
+++ app/Services/Api/Client/HomeService.php
@@ -33,7 +33,7 @@ class HomeService
 
     public function packages(){
 
-        $data['sliders'] = Package::select('id','price','title','description')->get();
+        $data['packages'] = Package::select('id','price','title','description')->get();
 
         return helperJson($data, '');
     }
